808a2bc88e2672da98be11774ec674e9e9b4197d
SH-ur <sdjsm2003@gmail.com>
2024-05-27 11:48:22 -0400

    Endpoint Bot Communication 1

--- Backend/udpStuff/updHandler.js
+++ Backend/udpStuff/updHandler.js
@@ -1,16 +1,19 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const prueba = require("../udpStuff/udpDePrueba");
 const updHan = Router();
 
-updHan.post('/communication', async (req, res) => {
-    try {
-        const {command} = req.body;
-        if(!command || typeof command !== 'string') return "I'm so sorry, for the moment we cannot succeed with the request."
-        const response = await prueba(command);
-        response ? res.status(200).json({ message: response }) : res.status(400).json({ message: `Something went wrong: ${response}` });
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+updHan.post("/communication", async (req, res) => {
+  try {
+    const { command } = req.body;
+    if (!command || typeof command !== "string")
+      return "I'm so sorry, for the moment we cannot succeed with the request.";
+    const response = await prueba(command);
+    response
+      ? res.status(200).json({ message: response })
+      : res.status(400).json({ message: `Something went wrong: ${response}` });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 module.exports = updHan;
