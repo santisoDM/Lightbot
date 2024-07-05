d9baad89fa4e84e97c2b9f471006ee146fd8935b
unknown <sdjsm2003@gmail.com>
2024-07-05 10:58:23 -0400

    fixing runtimeversion on appconfigjs again

--- Frontend/Lightapp/app.config.js
+++ Frontend/Lightapp/app.config.js
@@ -17,7 +17,7 @@ export default ({ config }) => {
       updates:{
         url: "https://u.expo.dev/aeb1e1d3-4c2d-4485-af21-315e3dd020bb",
       },
-      runtimeVersion:{policy: "34"},
+      runtimeVersion:{policy: "1.0.0"},
       ios: {
         supportsTablet: true,
         infoPlist: {
