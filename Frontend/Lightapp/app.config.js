7973696433039e9f59a775f1d5a59099f059bb35
unknown <sdjsm2003@gmail.com>
2024-07-05 10:52:59 -0400

    Fixing appconfig js

--- Frontend/Lightapp/app.config.js
+++ Frontend/Lightapp/app.config.js
@@ -17,6 +17,7 @@ export default ({ config }) => {
       updates:{
         url: "https://u.expo.dev/aeb1e1d3-4c2d-4485-af21-315e3dd020bb",
       },
+      runtimeVersion:{policy: "sdkVersion"},
       ios: {
         supportsTablet: true,
         infoPlist: {
